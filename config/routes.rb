0b413c92f0ca62341e129301edefe4fb2967dfce
Zumer Ahmad <macbookpro@Macbooks-MacBook-Pro.local>
2022-10-03 14:07:28 +0500

    route file edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   resources :posts
 
-  devise_for :users
-    root 'home_pages#homepage'
-    get 'homepage', to: 'home_pages#homepage'
-    get 'dashboard', to: 'home_pages#dashboard'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords'
+
+  }
+  root 'home_pages#homepage'
+  get 'homepage', to: 'home_pages#homepage'
+  get 'dashboard', to: 'home_pages#dashboard'
 
 end
